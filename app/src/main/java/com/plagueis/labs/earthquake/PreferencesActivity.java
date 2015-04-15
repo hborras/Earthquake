f2d77da5b548269c7f8599acad1fbcd63c2721a4
Hector Borras Aleixandre <hborras@cyberclick.net>
2015-04-15 19:25:24 +0200

    Content Provider finished

--- app/src/main/java/com/plagueis/labs/earthquake/PreferencesActivity.java
+++ app/src/main/java/com/plagueis/labs/earthquake/PreferencesActivity.java
@@ -5,17 +5,14 @@ import android.preference.PreferenceActivity;
 
 public class PreferencesActivity extends PreferenceActivity {
 
-    public static final String USER_PREFERENCE = "USER_PREFERENCE";
     public static final String PREF_AUTO_UPDATE = "PREF_AUTO_UPDATE";
-    public static final String PREF_MIN_MAG_INDEX = "PREF_MIN_MAG_INDEX";
-    public static final String PREF_UPDATE_FREQ_INDEX = "PREF_UPDATE_FREQ_INDEX";
-
     public static final String PREF_MIN_MAG = "PREF_MIN_MAG";
     public static final String PREF_UPDATE_FREQ = "PREF_UPDATE_FREQ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //noinspection deprecation Controled
         addPreferencesFromResource(R.xml.userpreferences);
     }
 }
